e3951960b802fcd05ca0015b484ba5ed79a44cdd
PrestonYoooHooo <89479634+PrestonYoooHooo@users.noreply.github.com>
2021-09-03 13:08:35 -0500

    Add files via upload

---
+++ # First Program for Game Design.py
@@ -0,0 +1,25 @@
+# First Program for Game Design
+# This is for comments
+
+# Preston Yoo
+# Date: 09/03/2021
+# This is for our first program. test average
+
+# Set variables
+test1=92
+test2=89
+test3=91
+test4=87
+
+#declare a average variable
+average=0
+#Four basic operators
+# Sum +
+# Sub -
+# Mult *
+# div /
+average= (test1+test2+test3+test4)/4
+# Let's print result
+
+#command or instuction using keywords
+print("The tests average is",average)

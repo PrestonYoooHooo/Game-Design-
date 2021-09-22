b75fbaed72db18a5045c3d669874b5b383eeed29
PrestonYoooHooo <prestondyoo@gmail.com>
2021-09-22 12:26:27 -0500

    l

--- Chal5.py
+++ Chal5.py
@@ -1,7 +1,7 @@
 import os, random
 os.system('cls')
-
 randy = random.randint(1,20)
+print("Hello! Welcome to guess a number!")
 for i in range(10):
     userInput=input("guess a number between 1-20")
     try:
@@ -17,11 +17,11 @@ for i in range(10):
             check=False
             if check: 
                 print("congratuations you got the number right please run again if you want to try again!")
-                os.killpg
+                print("You won in ", i+1)
+                break
         else:
             print("sorry that wasn't right, guess again!")
     else: 
         print("Sorry please enter a whole number")
-print("Sorry you are out of guesses the number is:")
-print(randy)
+print("Thank you for playing")
 
